a6991138da9445ba9b8c3f9f9619e89c103709ea
Chris Spiking <chrisspiking@gmail.com>
2017-03-01 17:07:42 +0000

    Initial commit

---
+++ src/main/java/uk/co/bitstyle/sbab/controllers/Main.java
@@ -0,0 +1,25 @@
+package uk.co.bitstyle.sbab.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Main controller rgistering handlers for index and then default any other html file name.
+ *
+ * @author cspiking
+ */
+@Controller
+public class Main {
+
+    @RequestMapping(value={"/", "index"})
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/src/{htmlFileName}.html")
+    public String staticFileSupport(@PathVariable String htmlFileName) {
+        return htmlFileName;
+    }
+
+}
